d3f01dcc0ee2d2a109f2bb391f1a440b795088ce
Debora Bucci <dbucci@protonmail.com>
2022-02-07 03:02:45 -0300

    use the state and effect hooks to implement the animation of the cart button only when items are added to the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsAnimated, setBtnIsAnimated] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce(
+  const numberOfCartItems = items.reduce(
     (curNumber, item) => curNumber + item.amount,
     0
   );
 
+  const btnClasses = `${classes.button} ${btnIsAnimated ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+
+    setBtnIsAnimated(true);
+    setTimeout(() => {
+      setBtnIsAnimated(false);
+    }, 300);
+  }, [items]);
+
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
