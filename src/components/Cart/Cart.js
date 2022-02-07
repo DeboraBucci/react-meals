477318464b71e47ac63bf99e384c45098553b616
Debora Bucci <dbucci@protonmail.com>
2022-02-07 02:40:02 -0300

    make the rest or remove logic work when pressing the '-' button inside the cart, it will remove the item if there is only one serving, and if there is more than one, it will get rested by one

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,6 +10,10 @@ const Cart = (props) => {
   const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
   const cartItemAddHandler = (item) => {
     cartCtx.addItem(item);
   };
@@ -22,6 +26,7 @@ const Cart = (props) => {
           name={item.name}
           amount={item.amount}
           price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
           onAdd={cartItemAddHandler.bind(null, item)}
         >
           {item.name}
