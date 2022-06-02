de83bf1259c50c1722d6e62f33d1261887acc4c0
Debora Bucci <dbucci@protonmail.com>
2022-06-02 12:23:02 -0300

    add loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,9 +5,12 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
+
       const response = await fetch(
         "https://react-http-21a3a-default-rtdb.firebaseio.com/meals.json"
       );
@@ -24,10 +27,18 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading)
+    return (
+      <section className={classes["meals-loading"]}>
+        <p>Loading...</p>
+      </section>
+    );
 
   const mealsList = meals.map((meal) => (
     <MealItem
